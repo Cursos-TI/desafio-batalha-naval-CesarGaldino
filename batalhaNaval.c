28631638faa6318bbed0e4d328950676b19786e5
CesarGaldino <cesargaldino1993@gmail.com>
2025-04-27 13:25:07 +0000

    TERMINEI TAREFA

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,43 +1,57 @@
 #include<stdio.h>
 
 
-
-int main() {
-    int tabuleiro[10][10];
-
-    // Inicializa o tabuleiro com 0
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+#include<stdio.h>
+    #define NAVIO 3
+    #define LINHAS 10
+    #define COLUNAS 10
+    int main() {
+        int tabuleiro[LINHAS][COLUNAS];
+    
+        // Inicializa o tabuleiro com 0
+        for (int i = 0; i < LINHAS; i++) {
+            for (int j = 0; j < COLUNAS; j++) {
+                tabuleiro[i][j] = 0;
+            }
         }
-    }
- // posicionar os navios no tabuleiro
- tabuleiro[0][0] = 3; // Navio 1
- tabuleiro[0][1] = 3; // Navio 1
- tabuleiro[0][2] = 3; // Navio 1
- tabuleiro[6][8] = 3; // Navio 2
- tabuleiro[7][8] = 3; // Navio 2
- tabuleiro[8][8] = 3; // Navio 2
-
-// Exibe o tabuleiro
-    printf("## Tabuleiro Batalha Naval ##\n   ");
-    printf("\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%2d ", i); // Exibe os números das colunas
-    }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++) {
-        printf("%2c ", 'A' + i); // Exibe as letras das linhas
-        for (int j = 0; j < 10; j++) {
-            printf("%2d ", tabuleiro[i][j]);
+        // cOLOCAR 1 NAVIO HORIZONTAL 1 NAVIO VERTICAL 2 NAVIOS NA DIAGONAL
+        // Coloca um navio horizontal na linha 0, começando na coluna 0
+        for (int j = 0; j < NAVIO; j++) {
+            tabuleiro[0][j] = 3;
+        }
+        // Coloca um navio vertical na coluna 9, começando na linha 0
+        for (int i = 0; i < NAVIO; i++) {
+            tabuleiro[i][9] = 3;
+        }
+        // Coloca dois navios na diagonal, começando na linha 5, coluna 5
+        for (int i = 0; i < NAVIO; i++) {
+            tabuleiro[5 + i][5 + i] = 3;
+        }
+        // Coloca um navio na diagonal, começando na linha 7, coluna 0
+        for (int i = 0; i < NAVIO; i++) {
+            tabuleiro[7 + i][0 + i] = 3;
+        }
+        // Exibe o tabuleiro
+        printf("   ");
+        for (int i = 0; i < LINHAS; i++) {
+            printf("%2d ", i); // Exibe os números das colunas
         }
         printf("\n");
-    }
+    
+        for (int i = 0; i < LINHAS; i++) {
+            printf("%2c ", 'A' + i); // Exibe as letras das linhas
+            for (int j = 0; j < COLUNAS; j++) {
+                printf("%2d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+
+
 
     return 0;
 }
 
+  
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
