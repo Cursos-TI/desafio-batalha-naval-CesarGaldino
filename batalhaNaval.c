46b5109d477fcf3cd257ec832dea3ffa5a559514
CesarGaldino <cesargaldino1993@gmail.com>
2025-05-05 00:54:13 +0000

    finalizei modo mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -45,7 +45,101 @@
             }
             printf("\n");
         }
+ // Inicializando matriz de habilidade de ataque em cone
+ int cone[3][5];
+ for (int i = 0; i < 3; i++) {
+     for (int j = 0; j < 5; j++) {
+         cone[i][j] = 0;
+     }
+ }
 
+ for (int i = 0; i < 3; i++) {
+     for (int j = 0; j < 5; j++) {
+         if (j == 2) {
+             for (int x = i; x > -1; x--) {
+                 cone[i][j + x] = 1;
+                 cone[i][j - x] = 1;
+             }
+         }
+     }
+ }
+
+
+
+ // Inicializando matriz de habilidade de ataque em cruz
+ int cruz[5][5];
+ for (int i = 0; i < 5; i++) {
+     for (int j = 0; j < 5; j++) {
+         if (i == 2 || j == 2) {
+             cruz[i][j] = 2;
+         } else {
+             cruz[i][j] = 0;
+         }
+     }
+ }
+
+
+ // Inicializando matriz de habilidade de ataque em octaedro
+ int octa[5][5];
+ for (int i = 0; i < 5; i++) {
+     for (int j = 0; j < 5; j++) {
+         if (i == 2 || j == 2) {
+             octa[i][j] = 3;
+         } else if ((i == 1 || i == 3) && (j == 1 || j == 3)) {
+             octa[i][j] = 3;
+         } else {
+             octa[i][j] = 0;
+         }
+     }
+ }
+
+ 
+ // Inserindo os ataques no tabuleiro
+ // Será gerado um tabuleiro só com a imagem dos ataques
+ int tabuComAtaq[10][10];
+ for (int i = 0; i < 10; i++) {
+     for (int j = 0; j < 10; j++) {
+         tabuComAtaq[i][j] = 0;
+     }
+ }
+
+ for (int i = 0, k = 0; i < 3; i++, k++) {
+     for (int j = 0, l = 0; j < 5; j++, l++) {
+         if (cone[i][j] != 0) {
+             tabuComAtaq[k][l] = cone[i][j];
+         }
+     }
+ }
+
+ for (int i = 0, k = 4; i < 5; i++, k++) {
+     for (int j = 0, l = 1; j < 5; j++, l++) {
+         if (cruz[i][j] != 0) {
+             tabuComAtaq[k][l] = cruz[i][j];
+         }
+     }
+ }
+
+ for (int i = 0, k = 2; i < 5; i++, k++) {
+     for (int j = 0, l = 4; j < 5; j++, l++) {
+         if (octa[i][j] != 0) {
+             tabuComAtaq[k][l] = octa[i][j];
+         }
+     }
+ }
+
+ // Exibindo tabuleiro com os ataques
+
+ printf("\nExibindo tabuleiro com os ataques.\n");
+ printf("(1) Ataque em cone.|(2) Ataque em cruz. |(3) Ataque em octaedro.\n");
+ printf("    A B C D E F G H I J\n");
+ for (int i = 0; i < 10; i++) {
+     i < 9 ? printf("%d  ", i + 1) : printf("%d ", i + 1);
+     for (int j = 0; j < 10; j++) {
+         printf("%d ", tabuComAtaq[i][j]);
+     }
+     printf("\n");
+ }
+ printf("\n");
 
 
     return 0;
